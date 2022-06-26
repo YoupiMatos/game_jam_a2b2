188988c748796a5f228f8e890c1607c6bbb2af49
matos <18725330+YoupiMatos@users.noreply.github.com>
2022-06-26 21:47:31 +0200

    Menu principal et intro
    Barkmout et sylvie
    Fix du lvl 3

--- Source/Actors/scie.gd
+++ Source/Actors/scie.gd
@@ -1,14 +1,18 @@
 extends Area2D
 
 
+
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-
+export var is_static = false
+onready var anim_player = $AnimationPlayer
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	if is_static:
+		anim_player.play("static")
+	else: anim_player.play("Idle")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
