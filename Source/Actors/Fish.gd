d0be2c4a5796f41dece174ed5d95797a3d3f0171
matos <18725330+YoupiMatos@users.noreply.github.com>
2022-06-26 15:31:42 +0200

    changement de niveau ok

--- Source/Actors/Fish.gd
+++ Source/Actors/Fish.gd
@@ -2,8 +2,10 @@ extends Node2D
 
 
 # Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+onready var anim_player = $Fish/AnimationPlayer
+onready var hitbox = $Fish/CollisionShape2D
+
+var ready = false
 
 
 # Called when the node enters the scene tree for the first time.
@@ -12,10 +14,17 @@ func _ready():
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
-
+func _process(delta):
+		if Autoload.level_2_corrupted:
+			if anim_player.current_animation == "jump":
+				anim_player.play("die")
+				hitbox.disabled = true
+	
 
 func _on_Fish_body_entered(body):
 	if body.is_in_group("player"):
 		get_tree().reload_current_scene()
+
+
+func _on_Fish_ready():
+	ready = true
--- Source/Autoload/Autoload.gd
+++ Source/Autoload/Autoload.gd
@@ -1,14 +1,19 @@
 extends Node
 
 var level_1_corrupted = false
+var level_2_corrupted = false
 var corruptions = 0
 
 func corrupt_level(level_name):
 	match level_name:
 		"level_1":
 			level_1_corrupted = true
+		"level_2":
+			level_1_corrupted = true
 
 func is_level_corrupted(level_name):
 	match level_name:
 		"level_1":
 			return true if level_1_corrupted else false
+		"level_2":
+			return true if level_2_corrupted else false
