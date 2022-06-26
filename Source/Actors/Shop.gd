fe4505f7ab1098372f9142a97bdea14d320f08c7
Siraplop <Siraplop@gmail.com>
2022-06-26 12:10:19 +0200

    Hello matos debut lvl 2

--- Source/Actors/Shop.gd
+++ Source/Actors/Shop.gd
@@ -18,5 +18,5 @@ func _ready():
 func _on_Shop_body_entered(body):
 	if body.is_in_group("player"):
 		anim_player.play("dialogue")
-		if body.Input.is_action_just_pressed("accept"):
+		if Input.is_action_just_pressed("accept"):
 			Autoload.level_1_corrupted = true
